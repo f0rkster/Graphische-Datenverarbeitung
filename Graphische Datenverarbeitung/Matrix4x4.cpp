b01c104fa0aff830e09edf0e55f2bf40517af27c
matthias gabel <matthias-gabel@hotmail.de>
2021-10-20 16:00:53 +0200

    version2

--- Graphische Datenverarbeitung/Matrix4x4.cpp
+++ Graphische Datenverarbeitung/Matrix4x4.cpp
@@ -11,23 +11,11 @@ Matrix4x4::Matrix4x4(float _V00, float _V01, float _V02, float _V03,
                     float _V10, float _V11, float _V12, float _V13,
                     float _V20, float _V21, float _V22, float _V23,
                     float _V30, float _V31, float _V32, float _V33)
+    : m_values{ {_V00, _V01, _V02, _V03}
+              , {_V10, _V11, _V12, _V13}
+              , {_V20, _V21, _V22, _V23}
+              , {_V30, _V31, _V32, _V33} }
 {
-    m_values[0][0] = _V00;
-    m_values[0][1] = _V01;
-    m_values[0][2] = _V02;
-    m_values[0][3] = _V03;
-    m_values[1][0] = _V10;
-    m_values[1][1] = _V11;
-    m_values[1][2] = _V12;
-    m_values[1][3] = _V13;
-    m_values[2][0] = _V20;
-    m_values[2][1] = _V21;
-    m_values[2][2] = _V22;
-    m_values[2][3] = _V23;
-    m_values[3][0] = _V30;
-    m_values[3][1] = _V31;
-    m_values[3][2] = _V32;
-    m_values[3][3] = _V33;
 }
 
 float Matrix4x4::GetValue(int _X, int _Y)
